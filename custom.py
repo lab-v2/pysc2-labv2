e68252f48d898d35e911b6270c1fc3ec763c5982
lahari1402 <59884581+lahari1402@users.noreply.github.com>
2023-04-16 22:56:11 +0000

    Code to add custom maps

---
+++ custom.py
@@ -0,0 +1,20 @@
+from pysc2.maps import lib
+
+
+class Custom(lib.Map):
+  filename = "Base_Map"
+  directory = ""
+  download = "C:/Program Files (x86)/StarCraft II/Maps/Test/"
+  players = 2
+  game_steps_per_episode = 16 * 60 * 30  # 30 minute limit.
+
+
+cus_maps = [
+    # "Empty128",  # Not really playable, but may be useful in the future.
+    "Map1",
+    "Base_Map",
+    "Base1"
+]
+
+for name in cus_maps:
+  globals()[name] = type(name, (Custom,), dict(filename=name))
