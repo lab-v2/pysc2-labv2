900d0c38471cb322bd178334121f029cb8c6ca80
Devendra Parkar <devendra.parkar@asu.edu>
2023-05-14 15:01:20 -0700

    new Config

--- custom.py
+++ custom.py
@@ -6,14 +6,15 @@ class Custom(lib.Map):
   directory = ""
   download = "C:/Program Files (x86)/StarCraft II/Maps/Test/"
   players = 2
-  game_steps_per_episode = 16 * 60 * 30  # 30 minute limit.
+  game_steps_per_episode = 16 * 60 * 3  # 3 minute limit.
 
 
 cus_maps = [
     # "Empty128",  # Not really playable, but may be useful in the future.
     "Map1",
     "Base_Map",
-    "Base1"
+    "Base1",
+    "Base_Map_no_attack"
 ]
 
 for name in cus_maps:
